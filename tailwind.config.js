ce9bc6a25c4b477485562fe879edc6862041e4bf
Khadimou <khadimou-rassoul.diop@ens.uvsq.fr>
2025-10-29 13:23:34 +0100

    chore: add brand colors to tailwind config and CSS variables

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'fetra-olive': {
+          DEFAULT: '#6B8E23', // olive-ish
+          50:  '#F4F7EE',
+          100: '#E9F0DD'
+        },
+        'fetra-pink': {
+          DEFAULT: '#F472B6', // pink accent
+          50:  '#FFF0F6',
+          100: '#FFE6F2'
+        }
+      },
+      borderRadius: {
+        'xl-2': '1.25rem'
+      }
+    }
+  },
+  plugins: [],
+};
